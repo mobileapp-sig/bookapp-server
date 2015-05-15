d5c7193eddf45fd2bdfc12e6b3a0d09a1e87da3c
Choi Kirin <choi.kirin@gmail.com>
2015-05-15 08:47:26 +0000

    rest framework basic setting

--- bookapp_server/settings.py
+++ bookapp_server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,6 +82,12 @@ DATABASES = {
             'read_default_file': os.path.join(BASE_DIR,'my.cnf'),
         }
     }
+}
+
+# Rest Framework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
 }
 
 
