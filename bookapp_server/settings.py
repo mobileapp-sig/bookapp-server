071ca693c3119b4654075b4b4731df4d451c85ca
Choi Kirin <choi.kirin@gmail.com>
2015-05-20 09:42:20 +0000

    facebook login button added

--- bookapp_server/settings.py
+++ bookapp_server/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'bookapp_server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -152,3 +152,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = (
+    #os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'bookapp_server/static'),
+)
