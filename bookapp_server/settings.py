50441694524a57b6574981b1cf830dc76b8cdd27
Choi Kirin <choi.kirin@gmail.com>
2015-05-20 08:47:13 +0000

    default auth setting 누락된 부분을 추가함. 로그인 됨 이제

--- bookapp_server/settings.py
+++ bookapp_server/settings.py
@@ -103,6 +103,8 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
         'rest_framework_social_oauth2.authentication.SocialAuthentication',
     ),
