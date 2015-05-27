46bdac563753fbd6e5f8ef994e1b7d870718f8f7
Choi Kirin <choi.kirin@gmail.com>
2015-05-27 11:35:19 +0000

    rename _isbn to isbn

--- books/models.py
+++ books/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User, Group
 
 class Book(models.Model):
-    _isbn = models.CharField(max_length=13)
+    isbn = models.CharField(max_length=13)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200, blank=True)
     publisher = models.CharField(max_length=200, blank=True)
     description = models.CharField(max_length=1000, blank=True)
     pubdate = models.DateField(blank=True)
+
+    def isbn(self, value):
+        self._isbn = value
 
 class BookRating(models.Model):
     user = models.ForeignKey(User)
